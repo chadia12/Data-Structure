2ea225358794d7316ee90ce77b570d91ff600eaa
chadia12 <80862580+chadia12@users.noreply.github.com>
2022-01-18 20:04:43 -0600

    1/18/2022

--- Homework/Assgnement6/src/Question1/TestSymbols.java
+++ Homework/Assgnement6/src/Question1/TestSymbols.java
@@ -2,17 +2,39 @@ package Question1;
 
 public class TestSymbols {
     public static void main(String[] args) {
-        Symbols<String> str =new Symbols<>();
-        str.push("{");
-        str.push("[");
-        str.push("(");
-        str.push(")");
-        str.push("]");
-        str.push("}");
+        System.out.println(balancedSymbols("{(2+3)]"));
 
+        System.out.println(balancedSymbols("[{(2+3)}]"));
 
-
-
-
+        System.out.println(balancedSymbols("[)(2+3}]"));
     }
+
+    public static boolean balancedSymbols(String sy) {
+        Symbols<Character> str = new Symbols<>();
+        for (int i = 0; i < sy.length(); i++) {
+            char s = sy.charAt(i);
+            if (s == '[' || s == '(' || s == '{') {
+                str.push(s);
+            }
+            else if (s == ']') {
+                if ( str.empty() || str.pop() != '[' ) {
+                    return false;
+                }
+            } else if (s == ')') {
+                if (str.empty() || str.pop() != '(' ) {
+                    return false;
+                }
+            } else if (s == '}') {
+                if (  str.empty() || str.pop() != '{' ) {
+                    return false;
+                }
+            }
+
+        }
+        if(str.empty())
+        return false;
+
+        return true;
+    }
+
 }
